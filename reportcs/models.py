4be017bd266dd5cc78f890188e46c13c219236e1
HERVEBORISMBOULONTSI <mherveboris@gmail.com>
2022-11-15 09:27:43 +0100

    r

--- reportcs/models.py
+++ reportcs/models.py
@@ -477,7 +477,6 @@ def cs_sales_amount_query(date_from=None, date_to=None, **kwargs):
 def new_cs_per_month_query(date_from=None, date_to=None, **kwargs):
     queryset = ()
     return {"data": list(queryset)}
-
 def cs_in_use_query(user, **kwargs):
     date_from = kwargs.get("date_from")
     date_to = kwargs.get("date_to")
@@ -517,39 +516,55 @@ def cs_in_use_query(user, **kwargs):
     print(dictBase)
     return dictBase
 
+  
 def closed_cs_query(user, **kwargs):
     date_from = kwargs.get("date_from")
     date_to = kwargs.get("date_to")
     hflocation = kwargs.get("hflocation")
 
     format = "%Y-%m-%d"
+
     date_from_object = datetime.datetime.strptime(date_from, format)
     date_from_str = date_from_object.strftime("%d/%m/%Y")
-    
+
     date_to_object = datetime.datetime.strptime(date_to, format)
     date_to_str = date_to_object.strftime("%d/%m/%Y")
-    
-    policyA = Policy.objects.filter(
-        validity_from__gte = date_from,
-        validity_to__gte = date_to,
-        status = 4
-        ).count()
-    policyB = Policy.objects.filter(
-        validity_from__gte = date_from,
-        validity_to__gte = date_to,
-        status = 8
-        ).count()
-    dictBase = {
+
+    dictBase =  {
         "dateFrom": date_from_str,
         "dateTo": date_to_str,
-        "post": str(policyA+policyB)
-    }
-    if hflocation and hflocation !="0":
+        }
+
+    dictGeo = {}
+    if hflocation and hflocation!="0" :
         hflocationObj = HealthFacility.objects.filter(
-            code = hflocation,
-            validity_to__isnull = True
+            code=hflocation,
+            validity_to__isnull=True
             ).first()
         dictBase["fosa"] = hflocationObj.name
+        dictGeo["health_facility"] = hflocationObj.audit_user_id
+
+
+    PolicyList = Policy.objects.filter(
+        validity_from__gte = date_from,
+        validity_to__lte = date_to,
+        **dictGeo
+        )
+
+    policyA = Policy.objects.filter(
+            validity_from__gte = date_from,
+            validity_to__lte = date_to,
+            status = 4
+        ).count()
+
+    policyB = Policy.objects.filter(
+            validity_from__gte = date_from,
+            validity_to__lte = date_to,  
+            status = 8
+        ).count()
+    dictBase["post"]= str(policyA+policyB)
+    
+    print(dictGeo)
     return dictBase
 
 def severe_malaria_cost_query(date_from=None, date_to=None, **kwargs):
